538ef9115eff34b109076ec13e57085981376da7
Havard52 <103666284+Havard52@users.noreply.github.com>
2025-04-03 11:05:01 +0200

    Index and feed - front end
    Index and feed - front end finishes

---
+++ index.js
@@ -0,0 +1,5 @@
+export const API_BASE_URL = 'https://v2.api.noroff.dev';
+export const API_AUTH_REGISTER = "/auth/create-api-key";
+export const API_AUTH_LOGIN = "/auth/login";
+export const API_SOCIAL_POSTS = "/api/v1/social/posts?sort=created&sortOrder=desc&_author=true&_comments=true&_reactions=true";
+export const API_SOCIAL_POST = "/api/v1/social/posts/";
